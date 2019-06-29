2507e10c786c6c29d5171046386b8c3bbe0b7c2f
IndieGameMaker <myleje@gmail.com>
2019-06-29 09:43:25 +0900

    .

--- SpaceShooter/Assets/02.Scripts/GameManager.cs
+++ SpaceShooter/Assets/02.Scripts/GameManager.cs
@@ -8,20 +8,29 @@ public class GameManager : MonoBehaviour
     public GameObject monsterPrefab;
     //몬스터의 생성 주기
     public float createTime = 3.0f;
+    //게임 종료 여부
+    public bool isGameOver = false;
 
     void Start()
     {
         points = GameObject.Find("SpawnPointGroup").GetComponentsInChildren<Transform>();
         //메소드를 반복해서 호출(실행) 시킴
-        InvokeRepeating("CreateMonster", 2.0f, createTime);
+        //InvokeRepeating("CreateMonster", 2.0f, createTime);
     }
 
-    void CreateMonster()
+    IEnumerator CreateMonster()
     {
-        //난수 발생
-        int idx = Random.Range(1, points.Length); //(1, 22) 1...21
-        //몬스터 생성
-        Instantiate(monsterPrefab, points[idx].position, points[idx].rotation);
+        yield return new WaitForSeconds(2.0f); //Delay time
+        
+        while(isGameOver == false)
+        {
+            //난수 발생
+            int idx = Random.Range(1, points.Length); //(1, 22) 1...21
+            //몬스터 생성
+            Instantiate(monsterPrefab, points[idx].position, points[idx].rotation);
+
+            yield return new WaitForSeconds(createTime);
+        }
     }
 
 }
