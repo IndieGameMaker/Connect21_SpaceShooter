0e9d3ced12a2a20709537722db5576dfebe8b249
IndieGameMaker <myleje@gmail.com>
2019-06-29 09:25:43 +0900

    .

--- SpaceShooter/Assets/02.Scripts/GameManager.cs
+++ SpaceShooter/Assets/02.Scripts/GameManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] points;
+    public GameObject monsterPrefab;
+
     void Start()
     {
         
