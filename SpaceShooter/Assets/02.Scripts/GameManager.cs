5b0fd3a11886ce3d66a630f4a323eb6f31efa065
IndieGameMaker <myleje@gmail.com>
2019-06-29 09:36:52 +0900

    .

--- SpaceShooter/Assets/02.Scripts/GameManager.cs
+++ SpaceShooter/Assets/02.Scripts/GameManager.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Transform[] points;
+    private Transform[] points;
     public GameObject monsterPrefab;
+    //몬스터의 생성 주기
+    public float createTime = 3.0f;
 
     void Start()
     {
         points = GameObject.Find("SpawnPointGroup").GetComponentsInChildren<Transform>();
+        //메소드를 반복해서 호출(실행) 시킴
+        InvokeRepeating("CreateMonster", 2.0f, createTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateMonster()
     {
-        
+        //난수 발생
+        int idx = Random.Range(1, points.Length); //(1, 22) 1...21
+        //몬스터 생성
+        Instantiate(monsterPrefab, points[idx].position, points[idx].rotation);
     }
+
 }
