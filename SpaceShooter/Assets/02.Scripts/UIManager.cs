8b55c9f6cbe567d4aaa2b8f22dadca0e4beb8aed
IndieGameMaker <myleje@gmail.com>
2019-06-29 14:47:46 +0900

    .

--- SpaceShooter/Assets/02.Scripts/UIManager.cs
+++ SpaceShooter/Assets/02.Scripts/UIManager.cs
@@ -5,6 +5,11 @@ public class UIManager : MonoBehaviour
 {
     public void OnClickStart()
     {
-        SceneManager.LoadScene("Play");
+        //기존 씬을 삭제한 후에 로딩
+        SceneManager.LoadScene("Level01", LoadSceneMode.Single); 
+        //기존 씬을 그대로 둔채로 중첩해서 로딩(Merge)
+        SceneManager.LoadScene("Play", LoadSceneMode.Additive);
+        //UI 씬 로딩
+        //SceneManager.LoadScene("UI", LoadSceneMode.Additive);
     }
 }
